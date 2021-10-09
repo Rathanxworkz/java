a6a8e9b32cfa8589a0ae9887df8f5779ccb04a46
unknown <rathankumarhr.xworkz@gmail.com>
2021-10-10 00:19:02 +0530

    https://github.com/Rathanxworkz/java.git

---
+++ handBag.java
@@ -0,0 +1,65 @@
+class handBag{
+	String color;
+	String price;
+	String brand;
+	int size;
+	String material;
+	float capacity;
+	
+	handBag(String color,String price){
+		System.out.println("handBag1 invoked");
+		String Color=color;
+		String Price=price;
+		System.out.println(Color);
+		System.out.println(Price);		
+	 }
+	 
+	 
+	handBag(){
+	    System.out.println("handBag2 invoked");
+	}
+	 
+	handBag(float capacity){
+	    System.out.println("handBag3 invoked");
+		System.out.println(capacity);
+		}
+		
+	
+	handBag(String material){
+	    System.out.println("handBag4 invoked");
+		System.out.println(material);
+		}		
+		
+	handBag(int size){
+	    System.out.println("handBag5 invoked");
+		System.out.println(size);
+		}
+		
+		
+	handBag(String brand,int size){
+	    System.out.println("handBag6 invoked");
+		System.out.println(brand,size);
+		}
+		
+	/*handBag(String color){
+	    System.out.println("handBag7 invoked");
+		System.out.println(color);
+		}
+		
+	handBag(String size,String brand){
+	    System.out.println("handBag8 invoked");
+		System.out.println(size,brand);
+		}
+		
+	handBag(String price,String material){
+	    System.out.println("handBag9 invoked");
+		System.out.println(price,material);
+		}
+		
+	handBag(String capacity,String color){
+	    System.out.println("handBag10 invoked");
+		System.out.println(capacity,color);
+		}	*/ 
+	 
+	 
+}
---
+++ handbagStarter.java
@@ -0,0 +1,14 @@
+class handbagStarter{
+	public static void main(String[] args){		
+		new handBag("black","250rs");
+		new handBag();
+		new handBag(15.8f);
+		new handBag("leather");
+		new handBag(85);
+		new handBag("vintage",35);
+		/*new handBag("red");
+		new handBag("130cm","nike");
+		new handBag("350rs","cotton");
+		new handBag("186cm","blue");*/
+	}
+}
